c78e7a61f10d13b0e6125caf0bb7b411b1b3fb18
Alex Covone <alexcovone@gmail.com>
2023-01-09 20:49:56 -0500

    Reverted connectionDB

--- server.js
+++ server.js
@@ -19,23 +19,24 @@ MongoClient.connect(dbConnectionStr)
         console.log(`Connected to ${dbName} database.`)
         db = client.db(dbName)
         const infoCollection = db.collection('player-info')
+
+    app.get('/', (request, response) => {
+        response.sendFile(__dirname + 'index.html')
+    })
+        
+    app.get('/api/:playerName', (request, response) => {
+        const playerName = request.params.playerName.toLowerCase()
+        
+        infoCollection.find({name: playerName}).toArray()
+        .then(results => {
+            console.log(results)
+            response.json(results[0])
+        })
+        .catch(error => console.error(error))
+    })
+    
 })
 .catch(error => console.error(error))
-
-app.get('/', (request, response) => {
-    response.sendFile(__dirname + 'index.html')
-})
-    
-app.get('/api/:playerName', (request, response) => {
-    const playerName = request.params.playerName.toLowerCase()
-    
-    infoCollection.find({name: playerName}).toArray()
-    .then(results => {
-        console.log(results)
-        response.json(results[0])
-    })
-    .catch(error => console.error(error))
-})
 
 app.listen(process.env.PORT || PORT, () =>{
     console.log(`Server is running on ${PORT}.`)
